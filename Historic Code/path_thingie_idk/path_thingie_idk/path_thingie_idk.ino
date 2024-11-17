dc8a25fed24beb3f3782e3e0bf71ab0de2c1e5d8
Arnav Sharma <as3485@cam.ac.uk>
2024-11-17 22:18:29 +0000

    Some fixes. I have written this code without sleeping for 40 hours. There will be errors. Someone please debug them.

--- Historic Code/path_thingie_idk/path_thingie_idk/path_thingie_idk.ino
+++ Historic Code/path_thingie_idk/path_thingie_idk/path_thingie_idk.ino
@@ -13,6 +13,10 @@ int green_led_pin;
 int left_motor_pin;
 int right_motor_pin;
 
+int count_split = 0; //Checks Split Count
+int count_right = 0; //Checks number of juncitons where right turn could be taken
+int count_left = 0; //checks number of junctions where left turn could be taken 
+
 Adafruit_DCMotor *Motor_Left = AFMS.getMotor(left_motor_pin);
 Adafruit_DCMotor *Motor_Right = AFMS.getMotor(right_motor_pin);
 
@@ -27,32 +31,30 @@ void setup() {
   pinMode(extreme_left_pin, INPUT);
   pinMode(blue_led_pin, INPUT);
   pinMode(green_led_pin, INPUT);
-
-  
-
-
 }
 
 void loop() {
   
-  move();
+  
+  mov_forward_until_split_or_turn();
+
 }
 
-void move() {
+void mov_forward_until_split_or_turn(){
   int extreme_right = digitalRead(extreme_right_pin);
   int right = digitalRead(right_pin);
   int left = digitalRead(left_pin);
   int extreme_left = digitalRead(extreme_left_pin);
-  
+
   while(extreme_right == 0 && right == 1 && left == 1 && extreme_left == 0){
     extreme_right = digitalRead(extreme_right_pin);
     right = digitalRead(right_pin);
     left = digitalRead(left_pin);
     extreme_left = digitalRead(extreme_left_pin);
+    Motor_Left->setSpeed(255);
+    Motor_Right->setSpeed(255);
     Motor_Left->run(FORWARD);
     Motor_Right->run(FORWARD);
-    Motor_Left->setSpeed(255);
-    Motor_Run->setSpeed(255);
 
     if(extreme_right == 0 && right == 0 && left == 1 && extreme_left == 0){ //Off coursed to rightside so turn to left
       Motor_Left->setSpeed(200);
@@ -74,34 +76,48 @@ void move() {
         extreme_left = digitalRead(extreme_left_pin);
       }
     }
-    else if(extreme_right == 1 && right == 1 && left == 1 && extreme_left ==0){ //Turn Right
-      Motor_Right->run(BACKWARD);
-      while(extreme_right != 0 && right != 1 && left != 1 && extreme_left != 0){
-        delay(10);
-        extreme_right = digitalRead(extreme_right_pin);
-        right = digitalRead(right_pin);
-        left = digitalRead(left_pin);
-        extreme_left = digitalRead(extreme_left_pin);
-      }
-    }
     else if(extreme_right == 0 && right == 1 && left == 1 && extreme_left ==1){ //Turn Left
-      Motor_Left->run(BACKWARD);
-      while(extreme_right != 0 && right != 1 && left != 1 && extreme_left != 0){
-        delay(10);
-        extreme_right = digitalRead(extreme_right_pin);
-        right = digitalRead(right_pin);
-        left = digitalRead(left_pin);
-        extreme_left = digitalRead(extreme_left_pin);
+      left_count = left_count + 1;
+      if (left_count == 1){
+        turn_left();
       }
     }
-    else if(extreme_right == 1 && right == 1 && left == 1 && extreme_left ==1){
-      Motor_Right->run(RELEASE);
-      Motor_Left->run(RELEASE);
+    else if(extreme_right == 1 && right == 1 && left == 1 && extreme_left ==0){ //Turn Right
+      right_count = right_count + 1;
+      if (right_count == 2 || right_count == 3){
+        turn_right();
+      }
     }
-    else if(extreme_right==0 && right == 0 && left == 0 && extreme_left == 0) {
+    else if(extreme_right==1 && right == 1 && left == 1 && extreme_left == 1) {
       Motor_Right->run(RELEASE);
       Motor_Left->run(RELEASE);
+      count_split = count_split + 1;
+      if(count_split == 1){
+        turn_left();
+      }
+      else if(count_split == 2){
+        turn_right();
+      }
     }
   }
+}
 
+void turn_right(){
+  Motor_Right->run(BACKWARD);
+    while(extreme_right != 0 && right != 1 && left != 1 && extreme_left != 0){
+      delay(10);
+      extreme_right = digitalRead(extreme_right_pin);
+      right = digitalRead(right_pin);
+      left = digitalRead(left_pin);
+      extreme_left = digitalRead(extreme_left_pin);
+}
+
+void turn_left(){
+  Motor_Right->run(BACKWARD);
+    while(extreme_right != 0 && right != 1 && left != 1 && extreme_left != 0){
+      delay(10);
+      extreme_right = digitalRead(extreme_right_pin);
+      right = digitalRead(right_pin);
+      left = digitalRead(left_pin);
+      extreme_left = digitalRead(extreme_left_pin);
 }
